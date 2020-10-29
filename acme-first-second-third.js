f494d2f9e97cf78d008a22a5c1bfe4bd3852d527
Jared Suresky <jsuresky@gmail.com>
2020-10-29 18:41:59 -0400

    second commit

--- acme-first-second-third.js
+++ acme-first-second-third.js
@@ -1,4 +1,11 @@
+//create a variable for lists tag
+const lists = document.getElementById("lists");
+
+//create a variable/array for all the children in lists
+const listsChildren = [...lists.children];
+
 const slots = ["first", "second", "third"];
+
 const users = [
   { id: 1, name: "moe", slot: "first" },
   { id: 2, name: "larry", slot: "second" },
@@ -12,7 +19,6 @@ const thirdBox = document.getElementById("third-list");
 
 function renderUsers() {
   for (let i = 0; i < users.length; i++) {
-    // let userSlot = users.slot
     const user = document.createElement("li");
     user.innerHTML = users[i].name;
     user.id = users[i].id;
@@ -27,7 +33,70 @@ function renderUsers() {
     }
   }
 }
+
+function clearUsers() {
+  //if any child of lists is a <li> then remove child
+  listsChildren.forEach((child) => {
+    [...child.children].forEach((childElem) => {
+      if (childElem.nodeName === "LI") {
+        child.removeChild(childElem);
+      }
+    });
+  });
+}
+
+lists.addEventListener("click", (event) => {
+  //if element is a <P>...
+  if (event.target.nodeName === "LI") {
+    //console.log(event.target.className)
+    //if LI has a classname called selected
+    if (event.target.className === "selected") {
+      //rename it to empty string and make it selected property false
+      event.target.className = "";
+      event.target.selected = false;
+    } else {
+      //otherwise, give it the selected class and create a selected property/rename with a true value
+      event.target.className = "selected";
+      event.target.selected = true;
+    }
+  }
+});
+
+//when clicked inside of lists...
+lists.addEventListener("click", (event) => {
+  //if a button is clicked..
+  if (event.target.nodeName === "BUTTON") {
+    //variable for the buttons parent
+    const parentNode = event.target.parentElement;
+    //variable for parents next sibling
+    const nextParent = parentNode.nextElementSibling;
+    //variable for parents previous sibling
+    const previousParent = parentNode.previousElementSibling;
+    //array of all siblings in the buttons box
+    const boxChildren = [...event.target.parentElement.children];
+
+    //for each sibling (or children of the box with the button tag)
+    boxChildren.forEach((child) => {
+      if (child.nodeName === "LI" && child.className === "selected") {
+        //rename the slot of each user to the next or previous parents id# name
+        if (event.target.className === "move-right") {
+          users[child.id - 1].slot = nextParent.id;
+          //console.log('this is child', child)
+        } else if (event.target.className === "move-left") {
+          users[child.id - 1].slot = previousParent.id;
+          //child.slot = previousParent.id
+          // console.log('this is child', child)
+          // console.log(users)
+        }
+      }
+      //now that users all have different slot names, append elements
+      renderUsers();
+    });
+  }
+});
+
 renderUsers();
+
 /*My Notes when you first hit the page, data should be  interpreted and injected
 into the site. 
 
